28f163ed51f9e3ed8abac4a49c2b1eacfbbc781d
Md. Uzzal Mia <129536126+uzzal2200@users.noreply.github.com>
2024-06-16 23:05:56 +0600

    Add files via upload

---
+++ Module 05 : Function/function.sql
@@ -0,0 +1,31 @@
+use programminghero;
+select upper('abc')
+select lower('ABC')
+select FirstName, upper(FirstName)
+from Employee
+select abs(-2)
+select degrees(60)
+select radians(90)
+select cos(0)
+ select acos(radians(4))
+ 
+ select ceil(25.6)
+ select floor(25.6)
+ select 10 div 2
+ select exp(10)
+ select exp(-10)
+ select greatest(2,3,1,46,23,44)
+ select least(3454,345,1,3)
+ 
+ select ln(10)
+ select log(10)
+  select log10(10)
+  select 5 mod 2
+  select pi()
+  select pow(2,5)
+  select rand()
+  select round(12.567)
+  select sqrt(4)
+
+  
+ 
---
+++ Module 05 : Function/group_function.sql
@@ -0,0 +1,14 @@
+select max(Age)
+from Employee
+
+select min(Age)
+from Employee
+
+select avg(Age)
+from Employee
+
+select count(Age)
+from Employee
+
+select sum(Age)
+from Employee
